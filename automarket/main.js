38ac8f9ae890ac17e12a194cb4989d8c620a0847
Norberto <norgit@protonmail.com>
2021-10-24 19:26:09 -0300

    Update

--- automarket/main.js
+++ automarket/main.js
@@ -1,84 +1,87 @@
-/**
- * Mostrar una ventana modal para informar algo.
- * 
- * @param {string} icon - icono de la ventana modal.
- * @param {string} title - titulo de la ventana modal.
- * @param {number} timer - tiempo que la ventana modal sera mostrada.
- * @param {string} position - posicion de la ventana modal.
-*/
-const Toast = (icon, title, timer, position = 'top-end') => {
-  const toast = Swal.mixin({
-    toast: true,
-    position,
-    showConfirmButton: false,
-    timer,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-  })
-  toast.fire({icon, title})
-}
-
-/**
- * Mostrar una ventana modal para informar algo.
- * 
- * @param {number} timer - tiempo que la ventana modal sera mostrada.
-*/
-const sendingForm = (timer = 3000) => {
-  Swal.fire({
-    title: 'Enviando consulta...',
-    timer,
-    didOpen: () => {
-      Swal.showLoading()
-    }
-  })
-}
-
-/**
- * Verificar que el email sea válido.
- * 
- * @param {Element} email - información del email a validar.
- * @returns un objeto con el resultado de la validacion
-*/
-const validateEmail = email => {
-  let valid = false
-  let msg = 'Email inválido'
-  let mail = email.value.trim()
-  if (mail === '') {
-    return { valid, msg: 'El email no debe estar vacío' }
+document.addEventListener('DOMContentLoaded', () => {
+  /**
+   * Mostrar una ventana modal para informar algo.
+   * 
+   * @param {string} icon - ícono de la ventana modal.
+   * @param {string} title - título de la ventana modal.
+   * @param {number} timer - tiempo que la ventana modal será mostrada.
+   * @param {string} position - posición de la ventana modal.
+  */
+  const Toast = ({icon = 'success', title = 'Correcto', timer = 3000, position = 'top-end'}) => {
+    const toast = Swal.mixin({
+      toast: true,
+      position,
+      showConfirmButton: false,
+      timer,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    toast.fire({icon, title})
   }
-  if (!mail.includes('@')) {
+
+  /**
+   * Mostrar una ventana modal para informar algo.
+   * 
+   * @param {string} title - título de la ventana modal.
+   * @param {number} timer - tiempo que la ventana modal será mostrada.
+  */
+  const sendingForm = ({title = 'Enviando...', timer = 3000}) => {
+    Swal.fire({
+      title,
+      timer,
+      didOpen: () => {
+        Swal.showLoading()
+      }
+    })
+  }
+
+  /**
+   * Verificar que el email sea válido.
+   * 
+   * @param {string} email - información del email a validar.
+   * @returns un objeto con el resultado de la validacion
+  */
+  const validateEmail = email => {
+    const emailValue = email.trim()
+    const valid = true
+    const msg = 'Email válido'
+    if (emailValue === '') {
+      return { valid: false, msg: 'El email no debe estar vacío' }
+    }
+    if (!emailValue.includes('@')) {
+      return { valid: false, msg: 'Email inválido' }
+    }
     return { valid, msg }
   }
-  return { valid: true, msg: 'Email válido' }
-}
 
-document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#queryform')
+
   if (form) {
     const userfullname = document.querySelector('#userfullname')
     const userstate = document.querySelector('#userstate')
-    const usermail = document.querySelector('#usermail')
     const userquery = document.querySelector('#userquery')
+    const usermail = document.querySelector('#usermail')
     form.addEventListener('submit', e => {
       e.preventDefault()
       if (userfullname.value.trim() === '' ||
           userstate.value.trim() === '' ||
           userquery.value.trim() === '') {
-        return Toast('warning', 'Ningún campo debe quedar vacío')
+        return Toast({icon: 'warning', title: 'Ningún campo debe quedar vacío'})
       }
-      const { valid, msg } = validateEmail(usermail)
+      const { valid, msg } = validateEmail(usermail.value)
       if (!valid) {
-        return Toast('warning', msg)
+        return Toast({icon: 'warning', title: msg})
       }
       const timer = 3000
-      sendingForm(timer)
+      sendingForm({title: 'Enviando consulta...', timer})
       setTimeout(() => {
-        Toast('success', 'Consulta enviada. Muchas gracias', timer)
+        Toast({title: 'Consulta enviada. ¡Muchas gracias!', timer})
         form.reset()
       }, timer)
     })
   }
+
 })
